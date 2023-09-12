e600e0459716f076df1c1c2717ffad70b220a011
FlavioLAMartins <flavio_martins11@hotmail.com>
2023-09-12 18:22:34 -0300

    ocultando a navegação do header na pagina de login

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,21 +1,31 @@
-import Styles from "./Header.module.css";
-import {Link} from "react-router-dom";
+import Styles from './Header.module.css'
+import { Link, useLocation } from 'react-router-dom'
 
 function Header() {
+  const location = useLocation()
 
-    return (
-    <nav className={Styles.header}> 
-        <Link to="/home">
-        <span> 📚 Biblio Manager<h6>O seu gerenciador de biblioteca </h6> </span>
-        </Link>
+  //Verificar se esta na pagina de login
+
+  const isLoginPage = location.pathname === '/'
+
+  return (
+    <nav className={Styles.header}>
+      <Link to="/home">
+        <span>
+          {' '}
+          📚 Biblio Manager<h6>O seu gerenciador de biblioteca </h6>{' '}
+        </span>
+      </Link>
+      {!isLoginPage && (
         <nav>
-        <Link to="/home">Home</Link>
-        <Link to="/livro">Livros</Link>
-        <Link to="/leitor">Leitor</Link>
-        <Link to="/retirada">Retirada</Link>
-        </nav>    
+          <Link to="/home">Home</Link>
+          <Link to="/livro">Livros</Link>
+          <Link to="/leitor">Leitor</Link>
+          <Link to="/retirada">Retirada</Link>
+        </nav>
+      )}
     </nav>
-    )
+  )
 }
 
-export default Header;
+export default Header
